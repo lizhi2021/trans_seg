6f696c7d0328a5bf3e90fb97339efaedffe443dc
RishengDeng <dengrisheng1997@gmail.com>
2021-05-19 12:51:04 +0800

    some tools

---
+++ tools/dividedata.py
@@ -0,0 +1,4 @@
+import os 
+import shutil
+
+path = '/media/drs/extra2/Datasets/mvi_seg'
--- tools/renamemask.py
+++ tools/renamemask.py
@@ -1,10 +1,11 @@
 import os 
 import shutil
 
-path = '/media/drs/extra2/Datasets/mvi_seg/mask'
+path = '/media/drs/extra2/Datasets/mvi_seg/liver_mask'
 
 for i in os.listdir(path):
     print(i)
-    new_name = i.split('.')[0] + 'mask.npy'
+    # new_name = i.split('.')[0] + 'mask.npy'
+    new_name = i.replace('_mask_', 'mask')
     os.rename(os.path.join(path, i), os.path.join(path, new_name))
     # shutil.move(os.path.join(path, i), os.path.join(path, 'mvi_seg', i))
